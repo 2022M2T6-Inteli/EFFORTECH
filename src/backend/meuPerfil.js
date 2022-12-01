eed8740f207b216abe26c02ac60bc8473b12767b
Yuri-Toledo <110365951+Yuri-Toledo@users.noreply.github.com>
2022-12-01 02:07:50 -0300

    Criptografia das senhas e Login integrado

--- src/backend/meuPerfil.js
+++ src/backend/meuPerfil.js
@@ -1,4 +1,3 @@
-var n_obras = 2;
 var perfil;
 
 const id_usuario = 2;
@@ -24,9 +23,18 @@ fetch('/usuario')
     })
 
 
-function adiciona_card(){
+fetch('/obras')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        obras = data;
+        obras.map
+    })
 
-    
+function adiciona_card() {
+
+
     if( n_obras % 2 == 0){
         document.getElementById("catalogo_obras").innerHTML += '<div class="row linha_obras" id="' + n_obras/2 + '"><div class="card_servicos offset-1 col-4"><div class="row"><img class="img_obra" src="../imgs/obra-mrv-campo-grande-ms-881485.jpg" alt=""></div><div class="container informacoes_card"><div class="row"><h3>' + `${obra.nome}` + '</h3></div><div class="row informacoes_baixo"><div class="container"><div class="row"><div class="col-6 feedback"><a href=""><img class="icon_feedback" src="../imgs/feedback-do-cliente.png" alt="feedback"></a></div><!-- <div class="offset-1 col-1"><hr id="vertical"></div> --><div class="col-6 nota_servico"><p>' + `${perfil.nota}` + '</p></div></div></div></div></div></div></div>';
         n_obras = n_obras + 1;
