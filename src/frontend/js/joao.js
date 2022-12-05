f6dc32ac2e0d1a371f087dfa4ce8b46a9ae181c6
JoaoVictorMontagna <Joao.Montagna@sou.inteli.edu.br>
2022-12-05 09:19:38 -0300

    Codigo Comentado

--- src/frontend/js/joao.js
+++ src/frontend/js/joao.js
@@ -1,7 +1,10 @@
+
+// variaveis para definir os objetos que serão usados no codigo
 const btnDropDown = document.querySelector('.btnDropDown');
 const dropDownIcon = btnDropDown.querySelector('span');
 const mobileMenu = document.querySelector('.mobileMenu');
 
+// função para abrir o menu dropdown ao clicar no logo no menu responsivo do celular
 btnDropDown.addEventListener('click',()=>{
     dropDownIcon.innerText=dropDownIcon.innerText=== 'menu'
     ? 'close'
@@ -10,6 +13,7 @@ btnDropDown.addEventListener('click',()=>{
     mobileMenu.classList.toggle('is-open');
 })
 
+// variaveis para definir os obejtos para o codigo de mduar a navbar ao estar logado ou nao
 const menuNavbar=document.querySelector('.menuNavbar');
 const loginNavbar=document.querySelector('.loginNavbar');
 const registrarNavbar=document.querySelector('.registrarNavbar');
@@ -19,14 +23,22 @@ const item3DropDown=document.querySelector('.item3DropDown');
 const item4DropDown=document.querySelector('.item4DropDown');
 const item5DropDown=document.querySelector('.item5DropDown');
 
+
+// variavel para ver se o cliente esta logado ou deslogado
+// 1 = deslogado, 2=logado
+
+
 let deslogado=1
 
+
+
 function mudarNav(){
-  
+//   ao clicar serão inseridos uma classe que mudarão o CSS dos elementos
     menuNavbar.classList.toggle('logado');
     loginNavbar.classList.toggle('logado');
     registrarNavbar.classList.toggle('logado');
 
+    // executa esse codigo se estiver deslogado
     if (deslogado==1){
         item5DropDown.classList.add('sumir');
         item1DropDown.innerText="Home";
@@ -39,6 +51,7 @@ function mudarNav(){
         item4DropDown.href="comoFunciona.html";
 
         deslogado=2
+            // executa esse codigo se estiver logado
     } else{
         item1DropDown.innerText="Home";
         item1DropDown.href="home.html";
@@ -61,7 +74,7 @@ function mudarNav(){
 const menu=document.getElementById("menuNavbarGrande");
 
 
-
+// aciona o menu dropdown
 function abrirMenu(){
     mobileMenu.classList.toggle('is-open');
 
